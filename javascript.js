5ed243d7b3b7dcb4774b714ca1f48920be25537f
Matt DePero <deperomd@gmail.com>
2015-03-11 18:43:19 -0400

    testing remote sockets

--- javascript.js
+++ javascript.js
@@ -94,7 +94,7 @@ var url = "ws://107.10.18.206:8000/"
 // +----------------------------------------+
 // |          Controller Methods            |
 // +----------------------------------------+
-var fps = 10,turnRadius = 5;//Bigger turn radius = wider turns
+var fps = 10,turnRadius = 30;//Bigger turn radius = wider turns
 
 var ay=0;
 turnRadius = 1.0/turnRadius;
