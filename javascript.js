3e393b87e62c50b23d8f7d8e27e034d07b339f09
Matt DePero <deperomd@gmail.com>
2015-03-11 19:47:41 -0400

    testing remote sockets

--- javascript.js
+++ javascript.js
@@ -190,7 +190,7 @@ function draw(){
 	$(".object").remove();
 	for(var i = 0;i< gameArray.length;i++){
 		var objArray = gameArray[i].split(",");
-		$("body").append('<div class="object">'+i+'/div>');
+		$("body").append('<div class="object">'+i+'</div>');
 		// $("body").append('<div class="object" id="'+i+'"></div>');
 		// $("#"+i).css("left", objArray[0]);
 		// $("#"+i).css("top", objArray[1]);
